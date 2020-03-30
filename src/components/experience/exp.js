91c09c909345d7dfebbf670c883106b58478cb66
albearli <yiliang6@illinois.edu>
2020-03-30 14:57:52 -0500

    Change experience title

--- src/components/experience/exp.js
+++ src/components/experience/exp.js
@@ -12,13 +12,13 @@ function Experience() {
         Experience
       </h1>
       <div data-aos="fade-up" className="jobs">
-        <Job title="Software Engineering Intern" company="Motorola Solutions Inc." start="May 2020" end="TBD" url="https://www.motorolasolutions.com/en_us.html"/>
+        <Job title="Software Engineer Intern" company="Motorola Solutions Inc." start="May 2020" end="TBD" url="https://www.motorolasolutions.com/en_us.html"/>
         <hr/>
         <Job title="Project Manager & Course Staff" company="CS 196: Freshman Honors" start="Jan. 2020" end="Present" url="http://cs196.cs.illinois.edu/"/>
         <hr/>
         <Job title="Software Engineering Co-op" company="Caterpillar Inc." start="Aug. 2019" end="May 2020" url="https://www.caterpillar.com/en.html"/>
         <hr/>
-        <Job title="Software Engineering Intern" company="Ameren" start="May 2019" end="Aug. 2019" url="https://www.ameren.com/"/>
+        <Job title="Software Engineer Intern" company="Ameren" start="May 2019" end="Aug. 2019" url="https://www.ameren.com/"/>
         <hr/>
         <Job title="Course Assistant" company="CS 125: Introduction to Computer Science" start="Jan. 2019" end="May 2019" url="https://cs125.cs.illinois.edu/"/>
       </div>
