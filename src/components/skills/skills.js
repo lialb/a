f69111e946ec276177af68b2320e3b04155a3808
albearli <yiliang6@illinois.edu>
2020-08-15 14:01:33 -0500

    Change skill icons from webp

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -26,14 +26,14 @@ export default class Skills extends React.Component {
         </div>
         <div data-aos="fade-up" data-aos-delay="200" className="tech">
           <h1 data-aos-delay="200" className="tech-label d-none d-sm-block">
-            Frameworks: 
+            Frameworks/Tools: 
           </h1>
           <Icon image="angular.png" name="Angular"/>
-          <Icon image="react.webp" name="React"/>
+          <Icon image="react.png" name="React"/>
           <Icon image="android.png" name="Android"/>
           <Icon image="flutter.png" name="Flutter"/>
           <Icon image="flask.png" name="Flask"/>
-          <Icon image="nodejs.webp" name="Node.js"/>
+          <Icon image="nodejs.jpg" name="Node.js"/>
           <Icon image="kubernetes.png" name="Kubernetes"/>
           {/* <Icon image="aws.png" name="AWS"/> */}
         </div>
