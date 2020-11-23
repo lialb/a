c706e7985016d08e96d2c452f42002c5676d3033
albearli <yiliang6@illinois.edu>
2020-11-22 23:38:19 -0600

    Update about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -23,9 +23,9 @@ export default class About extends Component {
           <p data-aos="fade-left" data-aos-delay="200">
             Studying <strong>Computer Science</strong> at The University of Illinois at Urbana-Champaign
           </p>
-          <p data-aos="fade-left" data-aos-delay="200" >
+          {/* <p data-aos="fade-left" data-aos-delay="200" >
             Pursuing two minors in <strong>Statistics</strong> and <strong>Mathematics</strong>
-          </p>
+          </p> */}
           <p data-aos="fade-left" data-aos-delay="200">
             Trying my best &#x3c;3
           </p>
@@ -33,32 +33,38 @@ export default class About extends Component {
         <hr width="70%"/>
         <div data-aos="fade-right" data-aos-delay="200" class="centered">
           <p class="text">
-            Incoming Software Engineer Intern at <strong>Salesforce Tableau</strong> for summer '21.
-            <br></br>
-            <br></br>
+            Incoming SWE Intern at <strong>Salesforce Tableau</strong> for summer '21
+            <br/>
+            Incoming SWE Intern at <strong>Facebook</strong> for fall '21
+            <br/>
+            <br/>
             As of November 2020, I am a Software Engineer Intern at <strong>Motorola Solutions</strong>. 
             I worked as part of the CirrusCentral Mobile team, developing the backend for a mobile application monitoring radio systems data 
-            with real time notifications streams from Kafka.
-            <br></br>
-            <br></br>
+            with real time notifications streams from Kafka
+            <br/>
+            <br/>
             I previously worked at <strong>Caterpillar</strong> and created a visualization and analytics web platform that
-            improved efficiency of data lookup and extraction for 500+ users (still growing!).
-            <br></br>
-            <br></br>
+            improved efficiency of data lookup and extraction for 700+ users (still growing!)
+            <br/>
+            <br/>
             I worked at <strong>Ameren</strong> in summer 2019 as part of the Innovation App Development team, redesigning a project management 
             web portal used by PMs, analysts, and executives
+            <br/>
+            <br/>
+            I've been on Course Infrastructure Staff for CS196: Freshman Honors and a Course Assistant for CS125: Intro to Computer Science.
           </p>
         </div>
         <hr width="70%"/>
         <div data-aos="fade-left" data-aos-delay="200" class="centered">
           <p class="text">
             When I'm not working or doing homework, I enjoy teaching, working out, learning new stuff, and cats.
-            <br></br>
-            I've been on Course Infrastructure Staff for CS196: Freshman Honors and a Course Assistant for CS125: Intro to Computer Science.
-            <br></br>
-            <br></br>
+            <br/>
+            <br/>
+            I love building stuff. Especially stuff that can impact people and make a difference in the world.
+            <br/>
+            <br/>
             I'm particularly proud of my <a href="https://lialbert.com/uiuc-diversity/" target="_blank" rel="noopener noreferrer"><strong>UIUC Diversity</strong> </a> 
-            project, where I interactively display 15 years of racial demographics of every major and college at the University of Illinois.
+            project, where I (along with Bryant Zhou) interactively display 15 years of racial demographics of every major and college at the University of Illinois.
           </p>
         </div>
       </div>
