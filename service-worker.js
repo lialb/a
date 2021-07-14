36cc790e48a94e6a7cf96dd1764a6606e5b4fe36
albearli <yiliang6@illinois.edu>
2021-07-13 18:16:42 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/a/precache-manifest.916aaf43705532857ba4457f52af5bff.js"
+  "/a/precache-manifest.5eb7d4162c42affa29c0c1547c66d9a0.js"
 );
 
 self.addEventListener('message', (event) => {
